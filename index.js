2c31330c3dd5de766142330da3598617ceaf58b8
hq-office <843804874@qq.com>
2019-03-08 21:25:23 +0800

    重新制作修改

--- index.js
+++ index.js
@@ -1,52 +1,107 @@
 const http = require('http');
 var cheerio = require('cheerio');
 var fs = require("fs")
-let jsonData = []
-let count=1
-const  maxPage=2
-function getData(url) {
-  console.log(`正在抓取第${count}页`)
-  http.get(
-    url,
-    function (req, res) {
-      let html = '';
-      req.on('data', function (data) {
-        html += data;
+let cateIds = [
+  2723, 2683,
+  3894,
+  2392,
+  2460,
+  2641,
+  3891,
+  2559,
+  3413,
+  2780,
+  3899
+] //大分类数组
+function getPageData(url) { //获取页面元素
+  return new Promise((reslove, reject) => {
+    http.get(
+      url,
+      function (req, res) {
+        let html = '';
+        req.on('data', function (data) {
+          html += data;
+        });
+        req.on('end', function () {
+          reslove(html)
+        });
       });
-      req.on('end', function () {
-        jsonData=jsonData.concat(createProductInfo(html))
-        count++
-        let url= `http://www.emall001.com/shop/cate-3525-0-0-0-0-0-0-0-0-${count}.html`
-        if(count<=maxPage){
-          getData(url)
-        }else{
-          console.log('正在写入....')
-          createFs(jsonData)
-        }
-      });
-    });  
+  })
+
 }
+
 function createProductInfo(data) {
   var $ = cheerio.load(data)
-  let info=[]
+  let info = []
   $('.list_pic .item').each((index, ele) => {
     let obj = {
       name: $(ele).find('.goods-pic img').attr('title'),
-      salePrice: $(ele).find('.sale-price').text().replace('¥',''),
-      marketPrice:$(ele).find('.market-price').text().replace('¥',''),
-      imgUrl:$(ele).find('.goods-pic img').attr('src'),
+      salePrice: $(ele).find('.sale-price').text().replace('¥', ''),
+      marketPrice: $(ele).find('.market-price').text().replace('¥', ''),
+      imgUrl: $(ele).find('.goods-pic img').attr('src'),
     }
     info.push(obj)
   });
   return info
 }
-getData('http://www.emall001.com/shop/cate-3525-0-0-0-0-0-0-0-0-1.html')
 
-function createFs(data){
-  fs.writeFile('玩具.json', JSON.stringify(data), function (err) {
+function createFs(fileName, data, options) {
+  fs.writeFile(fileName, JSON.stringify(data), options, function (err) {
     if (err) {
-        return console.error(err);
+      return console.error(err);
     }
-    console.log("数据写入成功！");
-})
+    console.log(`${fileName}数据写入成功...`);
+  })
 }
+
+function getCateMaxPage(pageData) { //获取分类页最大页码数
+  var $ = cheerio.load(pageData)
+  let url = $('.tc .pagination li').last().find('a').attr('href')
+  url = url.match(/\d{0,4}.html/g)
+  let maxAge = url[0].replace('.html', '')
+  console.log('当前分类最大页码:' + maxAge)
+  return Promise.resolve(maxAge)
+}
+
+function getCateUrls(url, cateId) { //获取分类的链接地址
+   getPageData(url).then(pageData => {
+    getCateMaxPage(pageData).then(maxAge => {
+      let cateUrls = []
+      for (let i = 0; i <= maxAge; i++) {
+        getCateDetails(`http://www.emall001.com/shop/cate-${cateId}-0-0-0-0-0-0-0-0-${maxAge}.html`)
+          .then(urls => {
+            console.log(`第${cateId}分类，第${i}页链接采集完成`)
+              setProductUrl(urls)
+          })
+      }
+    })
+  })
+}
+let gloalProductUrls=[]
+ function setProductUrl(urls){
+     gloalProductUrls.concat(urls)
+     console.log('数据写入成功')
+ }
+
+function getCateDetails(url) {
+  return new Promise(reslove => {
+    getPageData(url).then(pageData => {
+      let productUrls = []
+      var $ = cheerio.load(pageData)
+      $('.list_pic .item').each((index, ele) => {
+        productUrls.push($(ele).find('.goods-pic a').attr('href'))
+      })
+      console.log(`当前分类页商品数量:` + productUrls.length)
+      reslove(productUrls)
+    })
+  })
+}
+
+
+function init() {
+  cateIds.forEach(cateId => {
+    getCateUrls(`http://www.emall001.com/shop/cate-${cateId}-0-0-0-0-0-0-0-0-0.html`, cateId)
+  });
+  console.log(gloalProductUrls)
+}
+init()
